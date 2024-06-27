ee23ae1dd56b31f7aa27d2806c03155237b7e815
selaviu <roma45782@gmail.com>
2024-06-27 13:19:01 +0300

    Initial commit

---
+++ src/main/java/my/selaviu/WeatherMonitorApp/WeatherMonitorAppApplication.java
@@ -0,0 +1,13 @@
+package my.selaviu.WeatherMonitorApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherMonitorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherMonitorAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/selaviu/WeatherMonitorApp/WeatherMonitorAppApplicationTests.java
@@ -0,0 +1,13 @@
+package my.selaviu.WeatherMonitorApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherMonitorAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
