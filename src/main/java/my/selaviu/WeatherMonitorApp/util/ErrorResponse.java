18f7884bbe6ad994cdbffec6ea5b7fd9a45aed88
selaviu <roma45782@gmail.com>
2024-06-29 13:35:47 +0300

    Added ErrorResponse class for handling error responses

---
+++ src/main/java/my/selaviu/WeatherMonitorApp/util/ErrorResponse.java
@@ -0,0 +1,29 @@
+package my.selaviu.WeatherMonitorApp.util;
+
+public class ErrorResponse {
+
+    private String message;
+    
+    private Long timestamp;
+    
+    public ErrorResponse(String message, Long timestamp){
+        this.message = message;
+        this.timestamp = timestamp;
+    }
+    
+    public String getMessage() {
+        return message;
+    }
+    
+    public void setMessage(String message) {
+        this.message = message;
+    }
+    
+    public Long getTimestamp() {
+        return timestamp;
+    }
+    
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
