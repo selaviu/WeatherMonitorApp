0be102615869c0c7f51023831a30adcb1a63a055
selaviu <roma45782@gmail.com>
2024-06-29 13:40:34 +0300

    Added ModelMapper bean for object mapping in WeatherMonitorAppApplication

--- src/main/java/my/selaviu/WeatherMonitorApp/WeatherMonitorAppApplication.java
+++ src/main/java/my/selaviu/WeatherMonitorApp/WeatherMonitorAppApplication.java
@@ -1,7 +1,9 @@
 package my.selaviu.WeatherMonitorApp;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WeatherMonitorAppApplication {
@@ -9,5 +11,12 @@ public class WeatherMonitorAppApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherMonitorAppApplication.class, args);
 	}
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
+
+
 
 }
